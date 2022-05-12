007236e211c630cfcba5e2c9c9aeb0714b2ef47c
kyle.macasinag <kylenmac@verizon.net>
2022-05-12 15:06:13 -0400

    Imported Postman JSON. Removed unused code

--- backend/reply/views.py
+++ backend/reply/views.py
@@ -1,8 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
-from django.shortcuts import get_object_or_404
 from .models import Reply
 from .serializers import ReplySerializer
 # Create your views here.
